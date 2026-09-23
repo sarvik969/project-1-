5954e45aaa794fe2c914a620fd02548878d9cfde
sarvik969 <singhalsarvik@gmail.com>
2026-09-23 22:40:45 +0530

    Add files via upload

---
+++ q18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int p;
+    scanf("%d", &p);
+
+    if (p >= 90)
+        printf("Grade A");
+    else if (p >= 80)
+        printf("Grade B");
+    else if (p >= 70)
+        printf("Grade C");
+    else if (p >= 60)
+        printf("Grade D");
+    else
+        printf("Grade F");
+
+    return 0;
+}
